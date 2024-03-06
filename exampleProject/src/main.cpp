629967a4e3742f3e527e70a1686e5f9b90082f28
Svalorzen <svalorzen@gmail.com>
2024-03-06 11:13:32 +0100

    feat: improved test exe output

--- exampleProject/src/main.cpp
+++ exampleProject/src/main.cpp
@@ -3,11 +3,11 @@
 #include "GitHash.hpp"
 
 int main() {
-    std::cout << GitHash::branch << '\n';
-    std::cout << GitHash::sha1 << '\n';
-    std::cout << GitHash::shortSha1 << '\n';
-    std::cout << GitHash::dirty << '\n';
-    std::cout << GitHash::shortSha1 << (GitHash::dirty ? "-dirty" : "") << '\n';
+    std::cout << "Branch:     " << GitHash::branch << '\n';
+    std::cout << "Sha1:       " << GitHash::sha1 << '\n';
+    std::cout << "Short sha1: " << GitHash::shortSha1 << '\n';
+    std::cout << "Dirty?      " << GitHash::dirty << '\n';
+    std::cout << "Combined:   " << GitHash::shortSha1 << (GitHash::dirty ? "-dirty" : "") << '\n';
 
     return 0;
 }
