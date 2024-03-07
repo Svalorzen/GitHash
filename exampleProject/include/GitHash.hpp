f1ed7fe44555840ec2de6a08614c28d2eeab1357
Svalorzen <svalorzen@gmail.com>
2024-03-07 23:53:44 +0100

    fix: using soft link for GitHash header as well

--- exampleProject/include/GitHash.hpp
+++ exampleProject/include/GitHash.hpp
@@ -1,11 +1,1 @@
-#ifndef GIT_HASH_HEADER_FILE
-#define GIT_HASH_HEADER_FILE
-
-namespace GitHash {
-    extern const char * const branch;
-    extern const char * const sha1;
-    extern const char * const shortSha1;
-    extern const bool dirty;
-}
-
-#endif
+../../GitHash.hpp
